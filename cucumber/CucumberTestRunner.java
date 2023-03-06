7b1bc5825be313dd128a05c02eacb7c8d2a4ca8d
oleksandr <maestrik3@gmail.com>
2023-03-06 17:29:50 +0200

    fix Junit in pom and commented cucumber

--- cucumber/CucumberTestRunner.java
+++ cucumber/CucumberTestRunner.java
@@ -1,15 +1,15 @@
 package cucumber;
 //package org.cucumber;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+//import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "resources/features",
-        glue = "org.prog",
-        plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json",
-                "html:target/cucumber-report.html", "org.prog.cucumber.CucumberHooks"})
-public class CucumberTestRunner {
+//@RunWith(Cucumber.class)
+//@CucumberOptions(features = "resources/features",
+//        glue = "code",
+//        plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json",
+//                "html:target/cucumber-report.html", "org.prog.cucumber.CucumberHooks"})
+//public class CucumberTestRunner {
 
-}
+//}
